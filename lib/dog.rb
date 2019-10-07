3de5e589fa90b39154924191632fb595cc25a4e7
Ashley Twomey <aprier091012@gmail.com>
2019-10-07 13:11:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,18 +3,22 @@ class Dog
 	@@all = []
 	attr_accessor :name
 
-def initialize(name)	 
-    @name = name	    
-    @@all << self	    
-  end	  
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
 
-def self.all
+	def self.clear_all
+		@@all.clear
 
+	end
 
-def self.clear_all
-  @@clear_all
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+		
+  
+	end
 end
-
-def 
-end 
-end
+ 
