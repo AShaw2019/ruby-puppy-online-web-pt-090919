10842331c5f707db2205357a77c751b0ed3f39ea
Ashley Twomey <aprier091012@gmail.com>
2019-10-07 13:07:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+	@@all = []
+	attr_accessor :name
+
+def initialize(name)	  def initialize(name)
+    @name = name	    @name = name
+    @@all << self	    save
+  end	  end
+
+
+  def self.all	  def self.all
+@@ -20,4 +20,8 @@ def self.print_all
+  def self.clear_all	  def self.clear_all
+    @@all.clear	    @@all.clear
+  end	  end
+
+  def save
+    @@all << self
+  end
+end	end
